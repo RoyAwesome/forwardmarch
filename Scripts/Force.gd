65d0c590d2c449ceacda2286b3ac7ddee093257a
Garrett <kylegar@kylegar.net>
2024-11-18 22:58:28 -0800

    Add all the human units to the list.  clamp the build area

--- Scripts/Force.gd
+++ Scripts/Force.gd
@@ -20,6 +20,16 @@ func _init(player_id : int, on_team : Team):
 	Abilities = AbilityRunner.new(self)
 	ForceAbilities["Units"] = AbilitySet.new()
 	add_build_unit_ability(0, preload("res://Units/Human/Footman.tres"))
+	add_build_unit_ability(1, preload("res://Units/Human/Rifleman.tres"))
+	add_build_unit_ability(2, preload("res://Units/Human/Sorceress.tres"))
+	add_build_unit_ability(3, preload("res://Units/Human/MortarTeam.tres"))
+	add_build_unit_ability(4, preload("res://Units/Human/Priest.tres"))
+	add_build_unit_ability(5, preload("res://Units/Human/Spellbreaker.tres"))
+	add_build_unit_ability(6, preload("res://Units/Human/FlyingMachine.tres"))
+	add_build_unit_ability(7, preload("res://Units/Human/Dragonhawk.tres"))
+	add_build_unit_ability(8, preload("res://Units/Human/Knight.tres"))
+	add_build_unit_ability(9, preload("res://Units/Human/Tank.tres"))
+	add_build_unit_ability(10, preload("res://Units/Human/Gryphon.tres"))
 
 func add_build_unit_ability(index : int, unit_type: UnitResource):
 	if not unit_type:
--- Scripts/board.gd
+++ Scripts/board.gd
@@ -3,7 +3,20 @@ extends Node2D
 
 var OwningForce : Force
 
+const GridSize := Vector2i(16, 24)
+const GridSizeMinusOne := GridSize - Vector2i.ONE #used in bounds checking
+const BoardGridOffset := Vector2i(4, 1)
+const TileSizeInPixels := 32
+
 @onready var Camera : Camera2D = $Camera2D
+
+var cursor_unit_size := 0
+
+func global_position_to_tile_position(pos : Vector2) -> Vector2i:
+	var raw_pos := Vector2i((pos / TileSizeInPixels)) - BoardGridOffset
+	return raw_pos.clamp(Vector2i.ZERO, GridSizeMinusOne)
+func tile_position_to_global_position(pos : Vector2i) -> Vector2:
+	return (pos * TileSizeInPixels) + (BoardGridOffset * TileSizeInPixels)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,11 +28,16 @@ func _input(event: InputEvent) -> void:
 		OwningForce.push_input(event)
 	var mouse_input : InputEventMouseMotion = event as InputEventMouseMotion
 	if(mouse_input and %Cursor.visible):
-		%Cursor.global_position = floor(mouse_input.position / 32) * 32
+		var tile_position := global_position_to_tile_position(mouse_input.position)
+		var offset := (cursor_unit_size - 1) * Vector2i.ONE
+		if(tile_position.x + offset.x >= GridSizeMinusOne.x):
+			tile_position.x -= offset.x
+		if(tile_position.y + offset.y >= GridSizeMinusOne.y):
+			tile_position.y -= offset.y
+		%Cursor.global_position = tile_position_to_global_position(tile_position)
 		
 func get_cursor_grid_location() -> Vector2i:
-	var loc =  ($BaseLayer.global_position - %Cursor.global_position) / 32
-	return Vector2i(floor(loc))
+	return global_position_to_tile_position(%Cursor.global_position)
 
 func show_grid(should_be_visible : bool):
 	%GridLayer.visible = should_be_visible
@@ -30,6 +48,7 @@ func put_unit_on_cursor(unit : UnitResource):
 		return
 	%Cursor.visible = true
 	%Cursor.texture = unit.Sprite
+	cursor_unit_size = unit.UnitSize
 	%Cursor.scale = Vector2(unit.UnitSize * 32, unit.UnitSize * 32) / unit.Sprite.get_size() 
 	show_grid(true)
 	
